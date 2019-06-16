d58b349fa5401abe6e25277e6048a1acff109de8
ebros12 <35901597+ebros12@users.noreply.github.com>
2019-06-15 20:25:04 -0500

    Add files via upload

---
+++ server/models/countBodega.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+
+var countBodegaSchema = new Schema({
+    nombre: { type: String, required: [true, 'El nombre es necesario'] },
+    cantidad: { type: String, required: [true, 'El precio únitario es necesario'] },
+    bodega: { type: String, required: [true, 'la bodega es necesaria'] },
+
+    valor: { type: String, required: [true, 'el valor es necesariaa'] },
+});
+
+
+module.exports = mongoose.model('countBodega', countBodegaSchema);
